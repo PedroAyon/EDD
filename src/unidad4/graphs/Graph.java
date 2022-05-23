a999fd9766e49ea2efb38d90e6f7091e5f292456
PedroAyon <pedroayonb@gmail.com>
2022-05-23 18:50:45 -0500

    Adjacency Matrix Graph

---
+++ src/unidad4/graphs/Graph.java
@@ -0,0 +1,85 @@
+package unidad4.graphs;
+
+import java.util.ArrayList;
+
+public class Graph<T> {
+    private int numberOfVertices;
+    private final int maxNumberOfVertices;
+    private final boolean[][] matrix;
+    private ArrayList<T> tags;
+    private final boolean isDirected;
+
+    public Graph(Integer maxNumberOfVertices, Boolean isDirected) {
+        this.isDirected = isDirected;
+        this.maxNumberOfVertices = maxNumberOfVertices;
+        matrix = new boolean[this.maxNumberOfVertices + 1][this.maxNumberOfVertices + 1];
+        tags = new ArrayList<>();
+        tags.add(null);
+    }
+
+    public void insertEdge(int v1, int v2) {
+        if (v1 > numberOfVertices || v2 > numberOfVertices) {
+            System.out.println("Invalid operation.");
+            return;
+        }
+        matrix[v1][v2] = true;
+        if (!isDirected)
+            matrix[v2][v1] = true;
+    }
+
+    public void insertVertex(T tag) {
+        if (numberOfVertices >= maxNumberOfVertices) {
+            System.out.println("Graph is full.");
+            return;
+        }
+        tags.add(tag);
+        numberOfVertices++;
+    }
+
+    public void deleteVertex(T tag) {
+        int index = tags.indexOf(tag);
+        if (index == -1) {
+            System.out.println("Vertex " + tag + " does not exists.");
+            return;
+        }
+        deleteVertex(index);
+    }
+
+    public void deleteVertex(int v) {
+        if (v > numberOfVertices) {
+            System.out.println("Vertex " + v + " does not exists.");
+            return;
+        }
+        for (int i = v; i < numberOfVertices; i++)
+            for (int j = 1; j <= numberOfVertices; j++)
+                matrix[i][j] = matrix[i + 1][j];
+
+        for (int i = v; i < numberOfVertices; i++)
+            for (int j = 1; j <= numberOfVertices; j++)
+                matrix[j][i] = matrix[j][i + 1];
+
+        for (int i = 0; i <= numberOfVertices; i++) {
+            matrix[i][numberOfVertices] = false;
+            matrix[numberOfVertices][i] = false;
+        }
+        tags.remove(v);
+        numberOfVertices--;
+    }
+
+    public boolean isEmpty() {
+        return numberOfVertices == 0;
+    }
+
+    public boolean adjacency(int v1, int v2) {
+        return matrix[v1][v2] || matrix[v2][v1];
+    }
+
+    public void print() {
+        for (int i = 1; i <= numberOfVertices; i++) {
+            System.out.print(tags.get(i) + " ");
+            for (int j = 1; j <= numberOfVertices; j++)
+                System.out.print((matrix[i][j] ? '1' : '0') + " ");
+            System.out.println();
+        }
+    }
+}
---
+++ src/unidad4/graphs/Main.java
@@ -0,0 +1,29 @@
+package unidad4.graphs;
+
+public class Main {
+    public static void main(String[] args) {
+        Graph<Character> graph = new Graph<Character>(5, false);
+        graph.insertVertex('a');
+        graph.insertVertex('b');
+        graph.insertVertex('c');
+        graph.insertVertex('d');
+        graph.insertEdge(1, 2);
+        graph.insertEdge(3, 4);
+        graph.insertEdge(2, 3);
+        graph.print();
+        System.out.println();
+
+        graph.insertVertex('e');
+        graph.insertEdge(5, 4);
+        graph.insertEdge(3, 5);
+        graph.print();
+        System.out.println();
+
+        graph.deleteVertex(2);
+        graph.print();
+        System.out.println();
+
+        graph.insertVertex('f');
+        graph.print();
+    }
+}
